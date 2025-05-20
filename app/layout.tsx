695f09bedc079c9324bdef3b38babdf94782396d
Saile <eliasskaugeeriksen@gmail.com>
2025-05-20 13:17:59 +0200

    fix favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,37 +1,20 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
+import { ReactNode } from 'react';
 import './globals.css';
 
-const geistSans = Geist({
-	variable: '--font-geist-sans',
-	subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-	variable: '--font-geist-mono',
-	subsets: ['latin'],
-});
-
 export const metadata: Metadata = {
-	title: 'Quiz',
-	description: 'Quiz app',
+	title: 'Quiz App',
+	description: 'Test kunnskapen din med en quiz!',
 	icons: {
-		icon: '/saile-logo.png',
+		icon: [{ url: '/saile-logo.webp', type: 'image/png', sizes: '32x32' }],
 	},
 };
 
-export default function RootLayout({
-	children,
-}: Readonly<{
-	children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: ReactNode }) {
 	return (
-		<html lang='en'>
-			<body
-				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-			>
-				{children}
-			</body>
+		<html lang='no'>
+			<head />
+			<body>{children}</body>
 		</html>
 	);
 }
